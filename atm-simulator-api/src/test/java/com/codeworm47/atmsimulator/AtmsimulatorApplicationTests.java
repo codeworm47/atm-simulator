83b8ccbeef01c358e11772f6265100dacee6388a
Hesam <hesam.rasouli1@gmail.com>
2021-11-27 04:46:41 +0330

    init

---
+++ atm-simulator-api/src/test/java/com/codeworm47/atmsimulator/AtmsimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeworm47.atmsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmsimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
